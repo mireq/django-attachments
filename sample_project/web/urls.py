c2e3751bc13da02a0860e24effafd1f64aef11c2
Miroslav Bendík <miroslav.bendik@gmail.com>
2021-03-25 07:57:47 +0100

    More url replacements

--- sample_project/web/urls.py
+++ sample_project/web/urls.py
@@ -1,19 +1,18 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.conf import settings
-from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.urls import path
+
 from example import views as example_views
 
 
 urlpatterns = [
-	url(r'^admin/', admin.site.urls),
-	url(r'^$', example_views.IndexView.as_view(), name='home'),
-	url(r'^attachments/$', example_views.LiveUploadAttachments.as_view(), name='live_upload_attachments'),
-	url(r'^attachments-save/$', example_views.OnSaveUploadAttachments.as_view(), name='on_save_upload_attachments'),
-	url(r'^gallery/$', example_views.GalleryUpload.as_view(), name='gallery_upload'),
+	path('admin/', admin.site.urls),
+	path('', example_views.IndexView.as_view(), name='home'),
+	path('attachments/', example_views.LiveUploadAttachments.as_view(), name='live_upload_attachments'),
+	path('attachments-save/', example_views.OnSaveUploadAttachments.as_view(), name='on_save_upload_attachments'),
+	path('gallery/', example_views.GalleryUpload.as_view(), name='gallery_upload'),
 ]
 
 if settings.DEBUG:
