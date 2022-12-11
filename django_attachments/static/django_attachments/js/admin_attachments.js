12f1d0b2fa9d5a6adfdcdec34b9b732391a09ecb
Miroslav Bendík <miroslav.bendik@gmail.com>
2022-12-11 15:12:50 +0100

    fix: Save admin attachments only after submit

--- django_attachments/static/django_attachments/js/admin_attachments.js
+++ django_attachments/static/django_attachments/js/admin_attachments.js
@@ -1,6 +1,6 @@
 (function() {
 
-var updateUrls = function(input, widget) {
+function updateUrls(input, widget) {
 	var value = input.value;
 	if (value && value !== 'None') {
 		var listUrl = input.getAttribute('data-list-url');
@@ -25,38 +25,76 @@ var updateUrls = function(input, widget) {
 		widget.updateUrl = input.getAttribute('data-update-url');
 	}
 	widget.createLibraryUrl = input.getAttribute('data-create-library-url');
-};
+}
+
 
 document.addEventListener("DOMContentLoaded", function(event) {
-	var inputs = [];
-	Array.prototype.forEach.call(document.getElementsByClassName('django-attachments-widget'), function(input) {
-		inputs.push(input);
-	});
+	var inputs = Array.prototype.slice.call(document.getElementsByClassName('django-attachments-widget'));
+	var widgets = [];
 	inputs.forEach(function(input) {
 		var w = uploadWidget(input, {autoProcess: false});
+		widgets.push(w);
 		var creatingGallery = false;
 		updateUrls(input, w);
 		w.loadAttachments();
-		w.onChanged(function() {
-			if (creatingGallery) {
-				return;
+	});
+
+	var mainContent = document.getElementById('content-main');
+	var form = mainContent.getElementsByTagName('form')[0];
+
+	var interceptSubmit = true;
+	function onSubmit(e) {
+		if (!interceptSubmit) {
+			return;
+		}
+		var btn = e.submitter;
+		if (btn !== null) {
+			btn.disabled = true;
+		}
+		setTimeout(function() { btn.disabled = false; }, 5000);
+
+		var notSaved = inputs.length;
+		widgets.forEach(function(w, i) {
+			var input = inputs[i];
+
+			function saveAttachments() {
+				w.save(function() {
+					notSaved--;
+					if (notSaved === 0) {
+						if (btn !== null) {
+							btn.disabled = false;
+						}
+
+						interceptSubmit = false;
+						event = new SubmitEvent("submit", {
+							submitter: btn
+						});
+						form.dispatchEvent(event);
+						setTimeout(function() { interceptSubmit = true; }, 0);
+					}
+				});
 			}
-			creatingGallery = true;
+
 			if (input.value && input.value !== 'None') {
-				w.save();
+				saveAttachments();
 			}
 			else {
 				w.createLibrary(function(value) {
 					if (value !== null) {
 						input.value = value;
 						updateUrls(input, w);
+						saveAttachments();
 						w.autoProcess = true;
 						w.save();
 					}
 				});
 			}
 		});
-	});
+
+		e.preventDefault();
+	}
+
+	form.addEventListener('submit', onSubmit, true);
 });
 
 }());
