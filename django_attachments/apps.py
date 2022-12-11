50dda98acc234e4e124d497fcef6bbf4296984e8
Miroslav Bendík <miroslav.bendik@gmail.com>
2022-12-11 09:48:50 +0100

    fix: Set default auto field to prevent migration generateion

--- django_attachments/apps.py
+++ django_attachments/apps.py
@@ -6,6 +6,7 @@ from django.utils.translation import gettext_lazy as _
 class AppConfig(apps.AppConfig):
 	name = 'django_attachments'
 	verbose_name = _("Attachemnts")
+	default_auto_field = 'django.db.models.AutoField'
 
 	def ready(self):
 		from .cleanup import register_cleaner_for_model
